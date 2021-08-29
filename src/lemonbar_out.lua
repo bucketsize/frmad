f25249742e87da88040570f82d54619e61690684
JB <bucket.size@gmail.com>
2021-08-29 19:49:58 +0530

    simplified

--- src/lemonbar_out.lua
+++ src/lemonbar_out.lua
@@ -7,19 +7,32 @@ local formats = require("frmad.formats")
 local cachec = require("frmad.cachec")
 
 local Sym ={
-	cpu = "",
-	gpu = "g",
-	mem = "",
-	eth = "",
-	wifi = "",
-	disc = "",
-	clock = "",
-	battery = "",
-	AC = "",
-	snd = "",
-	snd_mute = "",
-	disabled = "",
-	temperature = "",
+	cpu = "Cpu",
+	gpu = "Gpu",
+	mem = "Mem",
+	eth = "Eth",
+	wifi = "Wlan",
+	disc = "Df",
+	clock = "Tz",
+	battery = "Bat ",
+	AC = "Pow ",
+	snd = "Snd",
+	snd_mute = "Snd",
+	disabled = "X",
+	temperature = "T ",
+	-- cpu = "",
+	-- gpu = "g",
+	-- mem = "",
+	-- eth = "",
+	-- wifi = "",
+	-- disc = "",
+	-- clock = "",
+	-- battery = "",
+	-- AC = "",
+	-- snd = "",
+	-- snd_mute = "",
+	-- disabled = "",
+	-- temperature = "",
 }
 
 function co_out()
@@ -38,15 +51,15 @@ function co_out()
 		local audio = util:if_else(otab['vol'] == nil or otab['vol'] < 1
 			, {icon = Sym["snd_mute"], val = ""}
 			, {icon = Sym["snd"], val = mtab["vol"]})
-		print(string.format("%%{l} %s %s %s %%{c} %s %s %%{r} %s %s | %s %s | %s %s | %s %s | %s %s | %s %s "
+		print(string.format("%%{l} %s %s %s %%{c} %s %s %%{r} %s%s  %s%s  %s%s  %s%s  %s%s  %s%s "
 				, mtab['weather_temperature'], mtab['weather_humidity'], mtab['weather_summary']
 				, Sym['clock'], os.date("%a %b %d, %Y | %H:%M")
-				, Sym['cpu'], mtab['cpu']
-				, Sym['mem'], mtab['mem']
+				, Sym['cpu']  , mtab['cpu']
+				, Sym['mem']  , mtab['mem']
 				, Sym['temperature'], mtab['cpu_temp']
-				, Sym['eth'], util:if_else(mtab['net_gateway']=="?", Sym['disabled'], "")
-				, audio.icon, audio.val
-				, power.icon, power.val))
+				, Sym['eth']  , util:if_else(mtab['net_gateway']=="?", Sym['disabled'], "")
+				, audio.icon  , audio.val
+				, power.icon  , power.val))
 		coroutine.yield()
 	end
 end
